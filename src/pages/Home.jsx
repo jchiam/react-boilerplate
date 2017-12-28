f724adbc92cd16f10856fd52d22e24713040d71a
Jonathan Chiam <jczjchiam@gmail.com>
2017-12-28 10:57:53 +0800

    Upgrade dependencies (171228)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 
 export default class Layout extends Component {
   render() {
-    return (
-      <div>
-        <h1>React Boilerplate</h1>
-      </div>
-    );
+    return <h1>React Boilerplate</h1>;
   }
 }
